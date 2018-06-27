ac001c6e51158e83245ab891b63d22f74f082a21
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-06-27 13:45:23 -0700

    carMake-router.test is done

---
+++ src/__test__/carMake-router.test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+import superagent from 'superagent';
+import faker from 'faker';
+import { startServer, stopServer } from '../lib/server';
+import CarMake from '../model/carMake';
+import pCreateMockMake from './lib/carMakeMock';
+
+const apiUrl = `http://localhost:${process.env.PORT}/api/carmakes`;
+
+beforeAll(startServer);
+afterAll(stopServer);
+afterEach(() => CarMake.remove({}));
+
+describe('POST /api/carmakes', () => {
+  const mockResource = {
+    name: faker.name.firstName(),
+    country: faker.address.country(),
+  };
+
+  test('200 POST for successful POST of a Car Make', () => {
+    return superagent.post(apiUrl)
+      .send(mockResource)
+      .then((res) => {
+        expect(res.status).toEqual(200);
+        expect(res.body.name).toEqual(mockResource.name);
+        expect(res.body.country).toEqual(mockResource.country);
+        expect(res.body._id).toBeTruthy();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
+});
+
+describe('GET /api/carmakes', () => {
+  test('200 GET for successful fetching of a car make', () => {
+    let returnedCarMake;
+    return pCreateMockMake()
+      .then((newCarMake) => {
+        returnedCarMake = newCarMake;
+        return superagent.get(`${apiUrl}/${newCarMake._id}`);
+      })
+      .then((res) => {
+        expect(res.status).toEqual(200);
+        expect(res.body.name).toEqual(returnedCarMake.name);
+        expect(res.body.country).toEqual(returnedCarMake.country);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
+});
+
